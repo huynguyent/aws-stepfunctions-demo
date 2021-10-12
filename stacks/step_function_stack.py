e5ced698e15133b622a020cf0591e04ddd0fb978
huynguyent <huy.ngtuan@gmail.com>
2021-10-12 22:11:48 +1100

    Rename infra to stacks

--- stacks/step_function_stack.py
+++ stacks/step_function_stack.py
@@ -5,7 +5,7 @@ import aws_cdk.aws_logs as aws_logs
 import aws_cdk.aws_stepfunctions as sfn
 from aws_cdk import core as cdk
 
-from infra import policies
+from stacks import policies
 
 from .state_machine_definition import create_state_machine
 
