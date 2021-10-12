35585f2bb4693f0e0ca4b4de5aec8be6ffc02a50
huynguyent <huy.ngtuan@gmail.com>
2021-10-12 23:46:17 +1100

    Integrate lamba into stepfunctions

---
+++ stacks/step_functions/lambdas.py
@@ -0,0 +1,22 @@
+from dataclasses import dataclass
+
+import aws_cdk.aws_lambda as lambda_
+import constructs
+
+
+@dataclass
+class LambdaFunctions:
+    hello_world: lambda_.Function
+
+
+def create_functions(scope: constructs.Construct) -> LambdaFunctions:
+    return LambdaFunctions(
+        hello_world=lambda_.Function(
+            scope,
+            id="ExampleLambda",
+            function_name="ExampleLambda",
+            handler="handlers.hello",
+            runtime=lambda_.Runtime.PYTHON_3_9,
+            code=lambda_.Code.from_asset("functions"),
+        )
+    )
--- stacks/step_functions/state_machine_definition.py
+++ stacks/step_functions/state_machine_definition.py
@@ -1,6 +1,13 @@
 import aws_cdk.aws_stepfunctions as sfn
+import aws_cdk.aws_stepfunctions_tasks as tasks
 import constructs
 
+from stacks.step_functions.lambdas import LambdaFunctions
 
-def create_state_machine(scope: constructs.Construct) -> sfn.IChainable:
-    return sfn.Pass(scope=scope, id="test_task")
+
+def create_state_machine(
+    scope: constructs.Construct, lambda_functions: LambdaFunctions
+) -> sfn.IChainable:
+    return tasks.LambdaInvoke(
+        scope=scope, id="hello_world", lambda_function=lambda_functions.hello_world
+    ).next(sfn.Pass(scope=scope, id="test_task"))
